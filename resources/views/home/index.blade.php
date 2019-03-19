c915fe903b84c72fb5b2a9beec33499039fff0c7
Siroyagix <holymcontroler@gmail.com>
2019-03-19 20:32:44 +0900

    レイアウト部分を汎用的に修正した

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,20 +1,22 @@
-<html>
-    <head>
-        @extends('layouts.yagisprofile')
-        <title>"Yagi's Profile"</title>
-    </head>
-    <body>
+@extends('layouts.app')
+
+@section('title',"Yagi's Profile")
+
+@section('content')
+{{-- ヘッダーナビ部分 --}}
         <div class="titleback">
      <a id="home"></a>
         <p class="title">"Yagi's Profile"</p>
         <ul class="clearfix ulset">
-            <li class="list1"><a href="#home">home</a></li>
+            <li class="list1"><a href="/">Home</a></li>
             <li class="list1"><a href="#Profile">Profile</a></li>
-            <li class="list1"><a href="#works">works</a></li>
+            <li class="list1"><a href="#works">Works</a></li>
             <li class="list1"><a href="#Contact">Contact</a></li>
         </ul>
         </div>
-        
+        {{--　ヘッダーナビ部分　--}}
+
+        {{-- プロファイル --}}        
         <a id="Profile"></a>
         <h1>Profile</h1>
         
@@ -28,7 +30,8 @@
                     <p style="text-align:left">はじめまして。ヤギカズマと申します。</br>
                         医療業界で激務に追われるうちに必要なのはICT化だと思い立ち</br>
                         プログラムの世界に飛び込みました。</br>
-                        未経験からの挑戦で現在、日々成長中です。</p>
+                        未経験からの挑戦で現在、日々成長中です。
+                    </p>
                     <h2>経験したことのある技術</h2>
                     <table>
                         <tr>
@@ -45,28 +48,41 @@
                     </table>
                 </td>
             </tr>
-        </table>
-        
+        </table>        
+       {{-- /プロファイル --}}
 
+       {{-- ワークス --}}
         <a id="works"></a>
-        <h1>works</h1>
+        <h1>Works</h1>
         <table class="table">
             <tr>
-                <th>バイタルチェック</th><th>書評ブログ</th>
+                <th>バイタルチェック</th>
+                <th>書評ブログ</th>
             </tr>
             <tr>
-                <td><img src="/images/scope.png" alt="scope" class="workicons"></td>
-                <td><img src="/images/library.png" alt="library" class="workicons"></td>
+                <td>
+                    <img src="/images/scope.png" alt="scope" class="workicons">
+                </td>
+                <td>
+                    <img src="/images/library.png" alt="library" class="workicons">
+                </td>
             </tr>
-        </table>                
+        </table> 
+        {{-- /ワークス --}}
 
+        {{-- コンタクト --}}
         <a id="Contact"></a>
         <div class="under">    
         <h1>Contact</h1>
         <ul class="list2">
-            <li>yagikaz@gmail.com</li>
-            <li>"https://github-myprofile.com"</li>
+            <li>
+                <a href="mailto:yagikaz@gmail.com">
+                        yagikaz@gmail.com
+                </a>
+            </li>
+            <li>
+                <a href="https://github-myprofile.com">https://github-myprofile.com</a>
+            </li>
         </ul>
     </div>
-    </body>
-</html>
+   @endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=divice-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="/css/myprofile.css">
+
+        <title>@yield('title', "Yagi's Profile")</title>
+        
+    </head>
+    <body>
+        @yield("content")       
+    </body>
+</html>
+
+
